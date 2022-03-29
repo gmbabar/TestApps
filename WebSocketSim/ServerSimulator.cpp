656587fc40616d4f1ea1ee482f8cb018e66e7ae7
Ghulam M. Babar <gbabar@galaxydigital.io>
2022-03-29 18:21:55 -0400

    Added makefile, did some indentation

--- WebSocketSim/ServerSimulator.cpp
+++ WebSocketSim/ServerSimulator.cpp
@@ -10,20 +10,18 @@ namespace beast = boost::beast;
 namespace websocket = beast::websocket;
 
 
-class Session{
+class Session {
 public:
 
     Session(tcp::socket && socket) : ws_(std::move(socket)){}
 
-    void Run()
-    {
+    void Run() {
         net::dispatch(ws_.get_executor(), beast::bind_front_handler(&Session::OnRun, this));
     }
 
 private:
 
-    void OnRun()
-    {
+    void OnRun() {
         ws_.set_option(
             websocket::stream_base::timeout::suggested(
                 beast::role_type::server));
@@ -32,38 +30,29 @@ private:
         this, boost::placeholders::_1));
     }
 
-    void OnAccept(beast::error_code ec)
-    {
-        if(ec)
-        {
+    void OnAccept(beast::error_code ec) {
+        if(ec) {
             cerr << "Accept : " << ec.message() << endl;
             return;
         }
         // doRead();
     }
 
-    void SetupRead()
-    {
+    void SetupRead() {
         ws_.async_read(buffer_, beast::bind_front_handler(&Session::ReadHandler,
         this, boost::placeholders::_1, boost::placeholders::_2));
     }
 
-    void ReadHandler(beast::error_code &ec, size_t bytesRead)
-    {
+    void ReadHandler(beast::error_code &ec, size_t bytesRead) {
         boost::ignore_unused(bytesRead);
 
-        if(ec == beast::websocket::error::closed)
-        {
+        if(ec == beast::websocket::error::closed) {
             cout << "Server Closed" << std::endl;
             return;
-        }
-        else if(ec)
-        {
+        } else if(ec) {
             cout << "Read : " << ec.message() << endl;
             return;
-        }
-        else
-        {
+        } else {
             ws_.text(ws_.got_text());
             std::string s1(boost::asio::buffer_cast<const char*>(buffer_.data()));
             std::cout << s1 << std::endl;
@@ -80,10 +69,8 @@ private:
         }
     }
 
-    void WriteHandler(beast::error_code ec, size_t readBytes)
-    {
-        if(ec)
-        {
+    void WriteHandler(beast::error_code ec, size_t readBytes) {
+        if(ec) {
             cerr << "Write : " << ec.message() << endl;
             return; 
         }
@@ -92,67 +79,55 @@ private:
         SetupRead();
     }
 
-
     websocket::stream<tcp::socket> ws_;
     beast::flat_buffer buffer_;
     beast::flat_buffer auth_;
 
 };
 
-
-
-
-class Acceptor{
+class Acceptor {
 public:
-    Acceptor(net::io_context &ctx, tcp::endpoint endpoint) : ctx_(ctx), sock_(net::make_strand(ctx)),
-    acceptor_(net::make_strand(ctx_))
-    {
+    Acceptor(net::io_context &ctx, tcp::endpoint endpoint) : 
+             ctx_(ctx), 
+             sock_(net::make_strand(ctx)),
+             acceptor_(net::make_strand(ctx_)) {
         beast::error_code ec;
 
         acceptor_.open(endpoint.protocol(), ec);
-        if(ec)
-        {
+        if(ec) {
             cerr << "Open : " << ec.message() << endl;
             return;
         }
 
         acceptor_.set_option(net::socket_base::reuse_address(true), ec);
-        if(ec)
-        {
+        if(ec) {
             cerr << "Set_option : " << ec.message() << endl;
             return;
         }
 
         acceptor_.bind(endpoint, ec);
-        if(ec)
-        {
+        if(ec) {
             cerr << "bind : " << ec.message() << endl;
             return;
         }
 
         acceptor_.listen(net::socket_base::max_listen_connections, ec);
-        if(ec)
-        {
+        if(ec) {
             cerr << "Listen : " << ec.message() << endl;
             return;
         }
     }
 
-    void Accept()
-    {
-
+    void Accept() {
     }
+
 private:
 
-    void DoAccept(beast::error_code ec)
-    {
+    void DoAccept(beast::error_code ec) {
         acceptor_.async_accept(sock_, std::bind(&Acceptor::DoAccept, this, std::placeholders::_1));
-        if(ec)
-        {
+        if(ec) {
             cerr << "Accept : " << ec.message() << endl;
-        }
-        else
-        {
+        } else {
 
         }
     }
@@ -160,12 +135,11 @@ private:
     net::io_context &ctx_;
     tcp::socket sock_;
     tcp::acceptor acceptor_;
-
 };
 
-int main()
-{
+int main() {
     /*Your Code Here*/
     
     return 0;
 }
+
