84621924638d7f5b31eddb998bf9ed3af441c5f4
Ghulam M. Babar <gbabar@galaxydigital.io>
2022-04-07 16:55:22 -0400

    SSL socket updated

--- WebSocketSim/websocket_server_ssl.cpp
+++ WebSocketSim/websocket_server_ssl.cpp
@@ -1,19 +1,4 @@
-//
-// Copyright (c) 2016-2019 Vinnie Falco (vinnie dot falco at gmail dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-// Official repository: https://github.com/boostorg/beast
-//
 
-//------------------------------------------------------------------------------
-//
-// Example: WebSocket SSL server, asynchronous
-//
-//------------------------------------------------------------------------------
-
-//#include "example/common/server_certificate.hpp"
 #include "common/server_certificate.hpp"
 
 #include <boost/beast/core.hpp>
@@ -22,6 +7,8 @@
 #include <boost/beast/websocket/ssl.hpp>
 #include <boost/asio/strand.hpp>
 #include <boost/asio/dispatch.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 #include <algorithm>
 #include <cstdlib>
 #include <functional>
@@ -41,72 +28,87 @@ using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 //------------------------------------------------------------------------------
 
 // Report a failure
-void
-fail(beast::error_code ec, char const* what)
+void fail(beast::error_code ec, char const* what)
 {
     std::cerr << what << ": " << ec.message() << "\n";
 }
 
 // Echoes back all received WebSocket messages
-class session : public std::enable_shared_from_this<session>
+struct Session : public std::enable_shared_from_this<Session>
 {
-    websocket::stream<
-        beast::ssl_stream<beast::tcp_stream>> ws_;
-    beast::flat_buffer buffer_;
-
-public:
     // Take ownership of the socket
-    session(tcp::socket&& socket, ssl::context& ctx)
-        : ws_(std::move(socket), ctx)
+    Session(tcp::socket&& aSocket, ssl::context& aCtx, net::io_context& anIoc)
+        : m_ws(std::move(aSocket), aCtx), m_timer(anIoc)
     {
+        // since the whole purpose is to drop session, set call to close session
+        // as soon as we start a new session.
+        m_timer.expires_from_now( boost::posix_time::seconds(5) );
+        m_timer.async_wait(boost::bind(&Session::close, this));
+    }
+
+    void close() {
+        // Close the WebSocket connection
+        m_ws.async_close(websocket::close_code::normal,
+            beast::bind_front_handler(
+                &Session::onClose,
+                shared_from_this()));
+    }
+
+    void onClose(beast::error_code ec)
+    {
+        if(ec)
+            return fail(ec, "close");
+
+        // If we get here then the connection is closed gracefully
+        std::cout << __func__ << std::endl;
     }
 
     // Get on the correct executor
-    void
-    run()
+
+    void run()
     {
         // We need to be executing within a strand to perform async operations
         // on the I/O objects in this session. Although not strictly necessary
         // for single-threaded contexts, this example code is written to be
         // thread-safe by default.
-        net::dispatch(ws_.get_executor(),
+        net::dispatch(m_ws.get_executor(),
             beast::bind_front_handler(
-                &session::on_run,
+                &Session::onRun,
                 shared_from_this()));
     }
 
     // Start the asynchronous operation
-    void
-    on_run()
+    
+    void onRun()
     {
         // Set the timeout.
-        beast::get_lowest_layer(ws_).expires_after(std::chrono::seconds(30));
+        beast::get_lowest_layer(m_ws).expires_after(std::chrono::seconds(30));
 
          // Perform the SSL handshake
-        ws_.next_layer().async_handshake(
+        m_ws.next_layer().async_handshake(
             ssl::stream_base::server,
             beast::bind_front_handler(
-                &session::on_handshake,
+                &Session::onHandshake,
                 shared_from_this()));
     }
 
-    void
-    on_handshake(beast::error_code ec)
+    
+    void onHandshake(beast::error_code ec)
     {
         if(ec)
             return fail(ec, "handshake");
 
         // Turn off the timeout on the tcp_stream, because
         // the websocket stream has its own timeout system.
-        beast::get_lowest_layer(ws_).expires_never();
+        beast::get_lowest_layer(m_ws).expires_never();
 
         // Set suggested timeout settings for the websocket
-        ws_.set_option(
+        m_ws.set_option(
             websocket::stream_base::timeout::suggested(
                 beast::role_type::server));
 
         // Set a decorator to change the Server of the handshake
-        ws_.set_option(websocket::stream_base::decorator(
+        m_ws.set_option(websocket::stream_base::decorator(
             [](websocket::response_type& res)
             {
                 res.set(http::field::server,
@@ -115,39 +117,38 @@ public:
             }));
 
         // Accept the websocket handshake
-        ws_.async_accept(
+        m_ws.async_accept(
             beast::bind_front_handler(
-                &session::on_accept,
+                &Session::onAccept,
                 shared_from_this()));
     }
 
-    void
-    on_accept(beast::error_code ec)
+    void onAccept(beast::error_code ec)
     {
+        std::cout << __func__ << ": new session" << std::endl;
         if(ec)
             return fail(ec, "accept");
 
         // Read a message
-        do_read();
+        doRead();
     }
 
-    void
-    do_read()
+    void doRead()
     {
         // Read a message into our buffer
-        ws_.async_read(
-            buffer_,
+        m_ws.async_read(
+            m_buffer,
             beast::bind_front_handler(
-                &session::on_read,
+                &Session::onRead,
                 shared_from_this()));
     }
 
-    void
-    on_read(
+    void onRead(
         beast::error_code ec,
-        std::size_t bytes_transferred)
+        std::size_t aBytesTransferred)
     {
-        boost::ignore_unused(bytes_transferred);
+        std::cout << __func__ << ": " << beast::make_printable(m_buffer.data()) << std::endl;
+        boost::ignore_unused(aBytesTransferred);
 
         // This indicates that the session was closed
         if(ec == websocket::error::closed)
@@ -157,54 +158,53 @@ public:
             fail(ec, "read");
 
         // Echo the message
-        ws_.text(ws_.got_text());
-        ws_.async_write(
-            buffer_.data(),
+        m_ws.text(m_ws.got_text());
+        m_ws.async_write(
+            m_buffer.data(),
             beast::bind_front_handler(
-                &session::on_write,
+                &Session::onWrite,
                 shared_from_this()));
     }
 
-    void
-    on_write(
+    void onWrite(
         beast::error_code ec,
-        std::size_t bytes_transferred)
+        std::size_t a_BytesTransferred)
     {
-        boost::ignore_unused(bytes_transferred);
+        boost::ignore_unused(a_BytesTransferred);
 
         if(ec)
             return fail(ec, "write");
 
         // Clear the buffer
-        buffer_.consume(buffer_.size());
+        m_buffer.consume(m_buffer.size());
 
         // Do another read
-        do_read();
+        doRead();
     }
+private:
+    websocket::stream<beast::ssl_stream<beast::tcp_stream>> m_ws;
+    beast::flat_buffer m_buffer;
+    boost::asio::deadline_timer m_timer;
 };
 
 //------------------------------------------------------------------------------
 
 // Accepts incoming connections and launches the sessions
-class listener : public std::enable_shared_from_this<listener>
+struct Listener : public std::enable_shared_from_this<Listener>
 {
-    net::io_context& ioc_;
-    ssl::context& ctx_;
-    tcp::acceptor acceptor_;
-
-public:
-    listener(
-        net::io_context& ioc,
-        ssl::context& ctx,
-        tcp::endpoint endpoint)
-        : ioc_(ioc)
-        , ctx_(ctx)
-        , acceptor_(net::make_strand(ioc))
+    Listener(
+        net::io_context& aIoc,
+        ssl::context& aCtx,
+        tcp::endpoint aEndPoint)
+        : m_ioc(aIoc)
+        , m_ctx(aCtx)
+        , m_acceptor(net::make_strand(aIoc))
     {
+        std::cout << __func__ << std::endl;
         beast::error_code ec;
 
         // Open the acceptor
-        acceptor_.open(endpoint.protocol(), ec);
+        m_acceptor.open(aEndPoint.protocol(), ec);
         if(ec)
         {
             fail(ec, "open");
@@ -212,7 +212,7 @@ public:
         }
 
         // Allow address reuse
-        acceptor_.set_option(net::socket_base::reuse_address(true), ec);
+        m_acceptor.set_option(net::socket_base::reuse_address(true), ec);
         if(ec)
         {
             fail(ec, "set_option");
@@ -220,7 +220,7 @@ public:
         }
 
         // Bind to the server address
-        acceptor_.bind(endpoint, ec);
+        m_acceptor.bind(aEndPoint, ec);
         if(ec)
         {
             fail(ec, "bind");
@@ -228,7 +228,7 @@ public:
         }
 
         // Start listening for connections
-        acceptor_.listen(
+        m_acceptor.listen(
             net::socket_base::max_listen_connections, ec);
         if(ec)
         {
@@ -238,27 +238,26 @@ public:
     }
 
     // Start accepting incoming connections
-    void
-    run()
+    void run()
     {
-        do_accept();
+        doAccept();
     }
 
 private:
-    void
-    do_accept()
+    void doAccept()
     {
         // The new connection gets its own strand
-        acceptor_.async_accept(
-            net::make_strand(ioc_),
+        m_acceptor.async_accept(
+            net::make_strand(m_ioc),
             beast::bind_front_handler(
-                &listener::on_accept,
+                &Listener::onAccept,
                 shared_from_this()));
     }
 
-    void
-    on_accept(beast::error_code ec, tcp::socket socket)
+    
+    void onAccept(beast::error_code ec, tcp::socket socket)
     {
+        std::cout << __func__ << ": new listener" << std::endl;
         if(ec)
         {
             fail(ec, "accept");
@@ -266,12 +265,16 @@ private:
         else
         {
             // Create the session and run it
-            std::make_shared<session>(std::move(socket), ctx_)->run();
+            std::make_shared<Session>(std::move(socket), m_ctx, m_ioc)->run();
         }
 
         // Accept another connection
-        do_accept();
+        doAccept();
     }
+
+    net::io_context& m_ioc;
+    ssl::context& m_ctx;
+    tcp::acceptor m_acceptor;
 };
 
 //------------------------------------------------------------------------------
@@ -301,7 +304,7 @@ int main(int argc, char* argv[])
     load_server_certificate(ctx);
 
     // Create and launch a listening port
-    std::make_shared<listener>(ioc, ctx, tcp::endpoint{address, port})->run();
+    std::make_shared<Listener>(ioc, ctx, tcp::endpoint{address, port})->run();
 
     // Run the I/O service on the requested number of threads
     std::vector<std::thread> v;
@@ -316,4 +319,3 @@ int main(int argc, char* argv[])
 
     return EXIT_SUCCESS;
 }
-
