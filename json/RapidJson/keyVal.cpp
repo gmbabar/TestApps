ea4a21ccccccc00509477b572940786f2beaf940
Haris-Mirza <harismirza44661@gmail.com>
2022-04-08 11:25:57 +0500

    Fixed Some Bugs

--- json/RapidJson/keyVal.cpp
+++ json/RapidJson/keyVal.cpp
@@ -19,7 +19,7 @@ string JsonParser(std::string jsStr, std::string keyToFind)
 		{
 			return document[keyToFind.c_str()].GetString();
 		}
-		else if(document[keyToFind.c_str()].IsNumber())
+		else if(document[keyToFind.c_str()].IsInt())
 		{
 			return to_string(document[keyToFind.c_str()].GetInt());
 		}
@@ -27,6 +27,12 @@ string JsonParser(std::string jsStr, std::string keyToFind)
 		{
 			document[keyToFind.c_str()].Accept(writer);
 			return sb.GetString();
+		}
+		else if(document[keyToFind.c_str()].IsFloat())
+		{
+			std::string floatVal = to_string(document[keyToFind.c_str()].GetFloat());
+			floatVal.resize(floatVal.size()-5);
+			return floatVal;
 		}
 		else
 		{
@@ -53,9 +59,13 @@ std::string ParseJsonArr(std::string arr, int index)
 			d[index].Accept(wr);
 			return sb.GetString();
 		}
-		else if(d[index].IsNumber())
+		else if(d[index].IsInt())
 		{
 			return to_string(d[index].GetInt());
+		}
+		else if(d[index].IsFloat())
+		{
+			return to_string(d[index].GetFloat());
 		}
 		else if(d[index].IsString())
 		{
@@ -76,6 +86,16 @@ std::string ParseJsonArr(std::string arr, int index)
 int main()
 {
 	string complexJson = R"({"jsonrpc":"2.0","method":"subscription","params":{"channel":"trades.future.BTC.raw","data":[{"trade_seq":68190894,"trade_id":"101049974","timestamp":1649373917627,"tick_direction":2,"price":43597.5,"mark_price":43596.86,"instrument_name":"BTC-PERPETUAL","index_price":43595.2,"direction":"sell","amount":100.0}]}})";
-	std::string val = JsonParser(ParseJsonArr(JsonParser(JsonParser(complexJson, "params"), "data"), 0), "instrument_name");
-	cout << "The Value Of Trande_seq Is : " << val << endl;
+	string sec = R"({"jsonrpc":"2.0","id":2,"result":["trades.future.BTC.raw"],"usIn":1649398839656428,"usOut":1649398839656555,"usDiff":127,"testnet":true})";
+	cout << "Json String : " << complexJson << endl << endl;
+	cout << "Second string : " << sec << endl;
+
+	std::string instrument = JsonParser(ParseJsonArr(JsonParser(JsonParser(complexJson, "params"), "data"), 0), "instrument_name");
+	std::string amount = JsonParser(ParseJsonArr(JsonParser(JsonParser(complexJson, "params"), "data"), 0), "amount");
+	std::string price = JsonParser(ParseJsonArr(JsonParser(JsonParser(complexJson, "params"), "data"), 0), "price");
+	
+	cout << "The Value of \"Instrument_name\" is : " << instrument << endl;
+	cout << "The Value of \"amount\" is : " << amount << endl;
+	cout << "The Value of \"price\" is : " << price << endl;
+
 }
