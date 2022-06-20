a27a90013171e803edd193281caa3866b7f4e1b8
gbabar-galaxy <gbabar@galaxydigital.io>
2022-06-20 16:59:15 +0530

    indentation

--- deribit/cpp/encoder/encoder.cpp
+++ deribit/cpp/encoder/encoder.cpp
@@ -11,19 +11,19 @@ int main()
     char bufferEnc[512];
     MessageHeader hdr(bufferHdr, sizeof(bufferHdr));
     hdr.wrap(bufferHdr, 0, 1, sizeof(bufferHdr))
-    .blockLength(Book::sbeBlockLength())
-    .templateId(Book::sbeTemplateId())
-    .schemaId(Book::sbeSchemaId())
-    .version(Book::sbeSchemaVersion())
-    .numGroups(1)
-    .numVarDataFields(0);
+       .blockLength(Book::sbeBlockLength())
+       .templateId(Book::sbeTemplateId())
+       .schemaId(Book::sbeSchemaId())
+       .version(Book::sbeSchemaVersion())
+       .numGroups(1)
+       .numVarDataFields(0);
     Book b1;
     b1.wrapForEncode(buffer, 0+hdr.encodedLength(), sizeof(buffer))
-    .instrumentId(124972)
-    .timestampMs(1652693458999)
-    .prevChangeId(11608177861)
-    .changeId(11608177872)
-    .isLast(deribit_multicast::YesNo::yes);
+       .instrumentId(124972)
+       .timestampMs(1652693458999)
+       .prevChangeId(11608177861)
+       .changeId(11608177872)
+       .isLast(deribit_multicast::YesNo::yes);
 
     std::cout << b1 << std::endl;
 }
