a6f9418a4d6bb8776d89e86a9c73ec41be278d09
harismirza31 <harismirza44661@gmail.com>
2022-06-23 00:22:58 +0500

    The Work That Needed To Be Done

--- deribit/cpp/encoder/encoder.cpp
+++ deribit/cpp/encoder/encoder.cpp
@@ -1,29 +1,40 @@
 #include <iostream>
 #include <boost/beast/core/detail/base64.hpp>
-#include "deribit_multicast/Book.h"
+#include "./deribit_multicast/Trades.h"
 
 using namespace deribit_multicast;
 
 int main()
 {
-    char buffer[256];
-    char bufferHdr[256];
-    char bufferEnc[512];
-    MessageHeader hdr(bufferHdr, sizeof(bufferHdr));
-    hdr.wrap(bufferHdr, 0, 1, sizeof(bufferHdr))
-       .blockLength(Book::sbeBlockLength())
-       .templateId(Book::sbeTemplateId())
-       .schemaId(Book::sbeSchemaId())
-       .version(Book::sbeSchemaVersion())
-       .numGroups(1)
-       .numVarDataFields(0);
-    Book b1;
-    b1.wrapForEncode(buffer, 0+hdr.encodedLength(), sizeof(buffer))
-       .instrumentId(124972)
-       .timestampMs(1652693458999)
-       .prevChangeId(11608177861)
-       .changeId(11608177872)
-       .isLast(deribit_multicast::YesNo::yes);
+   char buffer[256];
+   MessageHeader hdr(buffer, sizeof(buffer));
+   hdr.wrap(buffer, 0, 1, sizeof(buffer))
+      .blockLength(Trades::sbeBlockLength())
+      .templateId(Trades::sbeTemplateId())
+      .schemaId(Trades::sbeSchemaId())
+      .version(Trades::sbeSchemaVersion())
+      .numGroups(1)
+      .numVarDataFields(0);
+   
+   Trades trade;
+   //Everything Ok Just Need To Fix The Values Of Offset and Bufflength
+   trade.wrapForEncode(buffer, 0, sizeof(buffer)) // <--- Here 
+   .instrumentId(1)
+   .tradesList() // <----- And Here 
+   .direction(Direction::Value::sell)
+   .price(39344.25)
+   .amount(10)
+   .timestampMs(1655921357363 )
+   .markPrice(38815.96)
+   .indexPrice(38603.64)
+   .tradeSeq(392167)
+   .tradeId(1297362)
+   .tickDirection(TickDirection::Value::zerominus)
+   .liquidation(Liquidation::Value::none)
+   .iv(0)
+   .blockTradeId(0)
+   .comboTradeId(0);
 
-    std::cout << b1 << std::endl;
+   //Giving Error std::runtime_error buffer too short for next group index [E108]
+   std::cout << trade << std::endl;
 }
