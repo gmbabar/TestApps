4d414c2f4decd2fedaa02e40ba2c79787a38d5c0
Ghulam M. Babar <gbabar@galaxydigital.io>
2022-07-24 10:57:13 -0400

    added mcast encoder decoder for binary messages

--- deribit/decoder/decoder_multicast_binmsg.cpp
+++ deribit/decoder/decoder_multicast_binmsg.cpp
@@ -19,7 +19,7 @@ void printBuffer(const char *buffer, size_t size) {
    printf("\n");
 }
 
-size_t decode(int msgType, char *buffer, size_t offset, size_t buffLen, size_t blockLen, int version) {
+size_t decodeMsg(int msgType, char *buffer, size_t offset, size_t buffLen, size_t blockLen, int version) {
    // check template-id
    switch(msgType) {
    case 1000:
@@ -32,7 +32,7 @@ size_t decode(int msgType, char *buffer, size_t offset, size_t buffLen, size_t b
        auto instrNameLen = (int)buffer[offset++];
        std::string instrName(&buffer[offset], instrNameLen);
        std::cout << "instrumentNameLength: " << instrNameLen << std::endl;
-       std::cout << "instrumentName: " << instrName << std::endl;
+       std::cout << "instrumentName: " << instrName << std::endl << std::endl;
        offset += instrNameLen;
        // TODO:
        // - Construct symbol as advised in encoder.cpp
@@ -72,7 +72,7 @@ size_t decode(int msgType, char *buffer, size_t offset, size_t buffLen, size_t b
        offset += trades.sbeBlockLength();
        tradesList.wrapForDecode(buffer, (uint64_t*)&offset, version, buffLen);
        while (tradesList.hasNext()) {
-           std::cout << "tradesList: " << tradesList.next() << std::endl;
+           std::cout << "tradesList: " << tradesList.next() << std::endl << std::endl;
        }
        break;
    }
@@ -117,34 +117,34 @@ size_t decode(int msgType, char *buffer, size_t offset, size_t buffLen, size_t b
    return offset;
 }
 
-
-void parseMsg(const char *buffer) {
-    size_t buffLen = 0;
+void parseMsg(char *buffer, size_t length) {
+    size_t buffLen = length;
     size_t blockLen = 0;
     size_t offset = 0;
     int version = 1;
     int msgType = 0;
     deribit_multicast::MessageHeader header;
-    char binBuff[10*1024];
 
-    auto res = boost::beast::detail::base64::decode(binBuff, buffer, strlen(buffer));
-    buffLen = res.first;
+    // char binBuff[10*1024];
+    // auto res = boost::beast::detail::base64::decode(binBuff, buffer, strlen(buffer));
+    // buffLen = res.first;
     while(offset < buffLen) {
         // decode msg header.
-        header.wrap(binBuff, offset, version, buffLen);
+        header.wrap(buffer, offset, version, buffLen);
         // decode full msg.
         msgType = header.templateId();
         blockLen = header.blockLength();
-        offset = decode(msgType, binBuff, offset, buffLen, blockLen, version);
+        offset = decodeMsg(msgType, buffer, offset, buffLen, blockLen, version);
    }
 }
 
 void reciveAndDecode(int &sockFd, sockaddr_in &addr, socklen_t &size) {
-    char buffer[1024];
+    char buffer[10*1024];
+    size_t length;
     while (true) {
-        recvfrom(sockFd, buffer, sizeof(buffer), 0, (sockaddr*)&addr, &size);
-        std::cout << "[Data Recieved] " << buffer << std::endl;
-        parseMsg(buffer);
+        length = recvfrom(sockFd, buffer, sizeof(buffer), 0, (sockaddr*)&addr, &size);
+        std::cout << "Data Recieved: " << length << " bytes." << std::endl;
+        parseMsg(buffer, length);
     }   
 }
 
