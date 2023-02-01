8f530abae54c1d3c1fa2ad3a9e86ad519f905c73
HarisMirza <89724753+HarisMirza21@users.noreply.github.com>
2023-02-01 17:57:11 +0500

    Update BinanceSimulator.py

--- md2_scripts/BinanceSimulator.py
+++ md2_scripts/BinanceSimulator.py
@@ -1,25 +1,20 @@
-import websocket
+import asyncio
+import websockets
 import json
 
-def on_message(ws, message):
-    print("Received message: " + message)
-
-def on_error(ws, error):
-    print("Received error: " + error)
-
-def on_close(ws):
-    print("Connection closed")
-
-def on_open(ws):
+async def send_message(websocket):
     message = {"message": "Hello, Server!"}
-    ws.send(json.dumps(message))
+    await websocket.send(json.dumps(message))
     print("Sent message: " + json.dumps(message))
 
+async def receive_message(websocket):
+    message = await websocket.recv()
+    print("Received message: " + message)
+
+async def main():
+    async with websockets.connect("ws://localhost:9443") as websocket:
+        await send_message(websocket)
+        await receive_message(websocket)
+
 if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://echo.websocket.org/",
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-    ws.on_open = on_open
-    ws.run_forever()
+    asyncio.get_event_loop().run_until_complete(main())
