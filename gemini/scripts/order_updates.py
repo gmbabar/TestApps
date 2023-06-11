88d77b25806cb678d9c52f02a591c7908ed5fe14
Ghulam M. Babar <gbabar@galaxydigital.io>
2023-06-11 01:36:22 +0000

    updated gem lamx scripts

--- gemini/scripts/order_updates.py
+++ gemini/scripts/order_updates.py
@@ -17,6 +17,9 @@ def on_error(ws, error):
 def on_close(ws):
     print("### closed ###")
 
+def on_open(ws):
+    print("Opened connection")
+
 gemini_api_key = "account-8BDIOhP2VnBAzQHAn6DT"
 gemini_api_secret = "mXob3f85YcHu2KEQDxXeVSMtabL".encode()
 
@@ -25,9 +28,14 @@ encoded_payload = json.dumps(payload).encode()
 b64 = base64.b64encode(encoded_payload)
 signature = hmac.new(gemini_api_secret, b64, hashlib.sha384).hexdigest()
 
+print('Payload: ', payload)
 
+websocket.enableTrace(True)
 ws = websocket.WebSocketApp("wss://api.sandbox.gemini.com/v1/order/events",
                             on_message=on_message,
+                            on_open=on_open,
+                            on_error=on_error,
+                            on_close=on_close,
                             header={
                                 'X-GEMINI-PAYLOAD': b64.decode(),
                                 'X-GEMINI-APIKEY': gemini_api_key,
--- lmax/scripts/authentication.py
+++ lmax/scripts/authentication.py
@@ -4,22 +4,61 @@ import hashlib
 import hmac
 import time
 from base64 import b64decode, b64encode
-import random
+import datetime
+
+'''
+LmaxG-UAT:
+web-endpoint: https://account-api.london-uat.lmax.com
+client_key: 0d554c092803e2c1dca579a5ca3d09fa
+client_sec: ODHvLOuy81+lOPul56O8Ng==
+- - - - - -
+LmaxD-UAT:
+web-endpoint: https://account-api.digital-uat.lmax.com
+client_key: 27d74ff768ee430626df8341763a8de1
+client_sec: Fm1cu6WZpQDppCFDoROTVA==
+'''
+
+import textwrap
+def print_roundtrip(response, *args, **kwargs):
+    format_headers = lambda d: '\n'.join(f'{k}: {v}' for k, v in d.items())
+    print(textwrap.dedent('''
+        ---------------- request ----------------
+        {req.method} {req.url}
+        {reqhdrs}
+
+        {req.body}
+        ---------------- response ----------------
+        {res.status_code} {res.reason} {res.url}
+        {reshdrs}
+
+        {res.text}
+    ''').format(
+        req=response.request,
+        res=response,
+        reqhdrs=format_headers(response.request.headers),
+        reshdrs=format_headers(response.headers),
+    ))
 
 client_key_id = '27d74ff768ee430626df8341763a8de1'
 client_secret = 'Fm1cu6WZpQDppCFDoROTVA=='
-nonce = str(random.randint(1000, 9999))
-timestamp = str(int(time.time()))
+nonce = str(time.time())
+timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.000Z")
+
 signature = b64encode(hmac.new(b64decode(client_secret), msg = bytes(client_key_id + nonce + timestamp, 'utf-8'), digestmod = hashlib.sha256).digest()).decode('utf-8')
 
 API_URL = 'https://account-api.digital-uat.lmax.com'
-# client_key_id = '2a6aed0de9f875c2407bc693034b477f'
-# nonce = 'nonce'
-# timestamp = '2021-10-21T01:43:28.786Z'
+JSON = {'client_key_id': client_key_id, 'timestamp': timestamp, 'nonce': nonce, 'signature': signature}
 
-r = requests.post(API_URL + '/v1/authenticate', json = {'client_key_id': client_key_id, 'timestamp': timestamp, 'nonce': nonce, 'signature': signature})
-if r.status_code != 200:
-    raise Exception('Failed to authenticate: ' + r.text)
+resp = requests.post(API_URL + '/v1/authenticate', json = JSON, hooks={'response': print_roundtrip})
+if resp.status_code != 200:
+    raise Exception('Failed to authenticate: ' + resp.text)
 
-auth_token = r.json()['token']
-print(auth_token)
+# auth_token = req.json()['token']
+print(JSON)
+token = resp.json()['token']
+header = {'Authorization':f"Bearer {token}"}
+r=requests.get(API_URL + '/v1/account/wallets', headers=header, hooks={'response': print_roundtrip})
+# print(token)
+# print(header)
+# print(timestamp)
+print(r.json())
